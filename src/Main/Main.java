57d59b8c517b8509b035379cbc31d50cb1d2ba7d
CatalinIagar <78844371+CatalinIagar@users.noreply.github.com>
2021-10-20 09:03:00 +0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,13 +9,13 @@ import GUI.UserFrame.UserMenu;
 
 public class Main {
     public static void main(String[] args) {
-        Account.accounts.add(new Account("test1", "R64i29abc@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
-        Account.accounts.add(new Account("test2", "R64i29abc@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
-        Account.accounts.add(new Account("test3", "R64i29abc@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
-        Account.accounts.add(new Account("test4", "R64i29abc@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
-        Account.accounts.add(new Account("test5", "R64i29abc@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
-        Account.accounts.add(new Account("test6", "R64i29abc@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
-        Account.accounts.add(new Account("test7", "R64i29abc@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
+        Account.accounts.add(new Account("test1", "A65p96xyz@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
+        Account.accounts.add(new Account("test2", "A65p96xyz@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
+        Account.accounts.add(new Account("test3", "A65p96xyz@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
+        Account.accounts.add(new Account("test4", "A65p96xyz@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
+        Account.accounts.add(new Account("test5", "A65p96xyz@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
+        Account.accounts.add(new Account("test6", "A65p96xyz@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
+        Account.accounts.add(new Account("test7", "A65p96xyz@".toCharArray(), "asd", "asd", "5010623260052", "asd", "0734512363"));
 
         Car.cars.add(new Car("Dacia", "Logan", "Rosu"));
         Car.cars.add(new Car("VW", "Golf", "Negru"));
